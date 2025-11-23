3b3c7b4200e3f1b8ad69c832546f20012ddd9199
DLWI-afk <s1349373@live.hkmu.edu.hk>
2025-11-23 15:20:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const User = require('./models/User');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({
